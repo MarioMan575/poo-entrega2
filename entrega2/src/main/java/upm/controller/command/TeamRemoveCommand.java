c927d9b631bd6d691361bdf0f2b718339d47fb51
Mario Manzanares Gala <mmanzanaresgala@gmail.com>
2024-11-19 10:41:08 +0100

    TeamRemove hecho

--- entrega2/src/main/java/upm/controller/command/TeamRemoveCommand.java
+++ entrega2/src/main/java/upm/controller/command/TeamRemoveCommand.java
@@ -1,4 +1,29 @@
 package upm.controller.command;
 
-public class TeamRemoveCommand {
+import upm.controller.TeamController;
+
+public class TeamRemoveCommand extends Command{
+    private final TeamController controller;
+
+    public TeamRemoveCommand(TeamController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public String apply(String[] params) {
+        if (params.length != 1) {
+            return "Invalid number of arguments.";
+        }
+        boolean success = controller.deleteTeam(params[0]);
+        if (success) {
+            return "Team deleted successfully.";
+        } else {
+            return "Team does not exist.";
+        }
+    }
+
+    @Override
+    public String toStringCommand() {
+        return "team-remove [teamName]";
+    }
 }
