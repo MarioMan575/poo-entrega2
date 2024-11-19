57073a12f1685a95fb0e39c3f154d6fb385a1248
Mario Manzanares Gala <mmanzanaresgala@gmail.com>
2024-11-19 11:37:33 +0100

    Tournaments acabados

--- entrega2/src/main/java/upm/controller/command/TournamentDeleteCommand.java
+++ entrega2/src/main/java/upm/controller/command/TournamentDeleteCommand.java
@@ -1,4 +1,29 @@
 package upm.controller.command;
 
-public class TournamentDeleteCommand {
+import upm.controller.TournamentController;
+
+public class TournamentDeleteCommand extends Command{
+    private final TournamentController controller;
+
+    public TournamentDeleteCommand(TournamentController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public String apply(String[] params) {
+        if (params.length != 1) {
+            return "Invalid number of arguments.";
+        }
+        boolean success = controller.deleteTournament(params[0]);
+        if (success) {
+            return "Tournament deleted successfully.";
+        } else {
+            return "Tournament not found.";
+        }
+    }
+
+    @Override
+    public String toStringCommand() {
+        return "tournament-delete [tournamentName]";
+    }
 }
--- entrega2/src/main/java/upm/controller/command/TournamentRemoveCommand.java
+++ entrega2/src/main/java/upm/controller/command/TournamentRemoveCommand.java
@@ -1,4 +1,29 @@
 package upm.controller.command;
 
-public class TournamentRemoveCommand {
+import upm.controller.TournamentController;
+
+public class TournamentRemoveCommand extends Command{
+    private final TournamentController controller;
+
+    public TournamentRemoveCommand(TournamentController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public String apply(String[] params) {
+        if (params.length != 1) {
+            return "Invalid number of arguments.";
+        }
+        boolean success = controller.deleteTournament(params[0]);
+        if (success) {
+            return "Tournament deleted successfully.";
+        } else {
+            return "Tournament not found.";
+        }
+    }
+
+    @Override
+    public String toStringCommand() {
+        return "tournament-remove [tournamentName]";
+    }
 }
