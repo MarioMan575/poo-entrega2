dc22c2b66824d7e6d6b034dfbc971dae30335845
s-marquesandres <s.marques.andres@gmail.com>
2024-11-21 10:23:01 +0100

    Team, TeamController

--- entrega2/src/main/java/upm/controller/TeamController.java
+++ entrega2/src/main/java/upm/controller/TeamController.java
@@ -1,5 +1,6 @@
 package upm.controller;
 
+import upm.model.Player;
 import upm.model.Team;
 import upm.model.User;
 
@@ -12,13 +13,13 @@ public class TeamController {
         teams = new HashMap<>();
     }
 
-    public boolean createTeam(String nombre) {
+    public String createTeam(String nombre, String leaderName) {
         if (teams.containsKey(nombre)){
-            return false;
+            return "Error: Team with this name already exits.";
         }
-        Team team = new Team(nombre);
+        Team team = new Team(nombre,leaderName);
         teams.put(nombre, team);
-        return true;
+        return "Success: Team created successfully.";
     }
 
     public Team getTeam(String nombre) {
@@ -27,5 +28,9 @@ public class TeamController {
 
     public boolean deleteTeam(String nombre) {
         return teams.remove(nombre) != null;
+    }
+    public String addTeam(String teamName,String playerEmail){
+
+        return null;
     }
 }
--- entrega2/src/main/java/upm/model/Team.java
+++ entrega2/src/main/java/upm/model/Team.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 public class Team {
     private String name;
+    private String leaderName;
     private List<Player> players;
 
-    public Team(String name) {
+    public Team(String name, String leaderName) {
         this.name = name;
         this.players = new ArrayList<>();
+        this.leaderName = leaderName;
+
+
     }
 
     public String getName() {
